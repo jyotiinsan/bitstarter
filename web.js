46e096afdb8b15a653c41f36f67df5eddbe04e7c
jyotiinsan <tojyoti@yahoo.com>
2013-08-08 12:11:19 +0000

    pp

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 
-var fs=requier('fs');
+var fs=require('fs');
 var express = require('express');
 var htmlfile="index.html";
 
-var app = express.createServer(express.logger));
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var html=fs.readFileSync(htmlfile).toString();
